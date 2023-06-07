ee22c74e2d7e80a4a59af20b06d8ca0b3e097918
Saul Paredes <saulkurosaki13@gmail.com>
2023-06-06 21:38:27 -0600

    Reducer update and Game Delete Button Done

--- src/components/MisJuegos.js
+++ src/components/MisJuegos.js
@@ -21,17 +21,41 @@ export const MisJuegos = () => {
             titulo: e.target.titulo.value,
             descripción: e.target.descripción.value,
         };
+
+        const action = {
+            type: 'crear',
+            payload: juego,
+        };
+
+        dispatch(action);
+    };
+
+    const borrarJuego = (id) => {
+        const action = {
+            type: 'borrar',
+            payload: id,
+        };
+
+        dispatch(action);
     };
 
   return (
     <div>
         <h1>Estos son mis Videojuegos</h1>
 
-        <p>Numero de Videojuegos: 15</p>
+        <p>Numero de Videojuegos: {juegos.length}</p>
         <ul>
-            <li>GTA</li>
-            <li>Mortal Kombat</li>
-            <li>Crash Bandicoot</li>
+            {
+                juegos.map(juego => (
+                    <li key={juego.id}>
+                        {juego.titulo}
+
+                        &nbsp;  <button type='button' onClick={() => borrarJuego(juego.id)}>
+                                    X
+                                </button>
+                    </li>
+                ))
+            }
         </ul>
 
         <h3>Agregar Juegos</h3>
--- src/reducers/JuegoReducer.js
+++ src/reducers/JuegoReducer.js
@@ -2,7 +2,10 @@ export const JuegoReducer = (state = [], action) => {
   
     switch (action.type) {
         case "crear":
-            return [...state, action.payload]
+            return [...state, action.payload];
+        
+        case "borrar":
+            return state.filter(juego => juego.id !== action.payload);
     
         default:
             return state;
